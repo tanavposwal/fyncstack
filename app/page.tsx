e06aa526997626501350eb8a3b4d85568b7ecc52
tanavposwal <tanavposwal939@gmail.com>
2024-12-04 15:18:22 +0530

    ui brush

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center font-[family-name:var(--font-geist-sans)] gap-10 bg-gradient-to-b from-white to-orange-100 ">
+    <div className="w-full h-screen flex flex-col items-center justify-center font-[family-name:var(--font-geist-sans)] gap-10 grainy select-none">
       <div className="w-full flex flex-col justify-center items-center gap-3">
-        <h1 className="text-6xl font-extrabold whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-black to-neutral-600">
+        <h1 className="md:text-6xl sm:text-5xl text-4xl font-extrabold">
           FYNC stack
         </h1>
-        <p className="text-sm text-neutral-600">with technolgies</p>
+        <p className="sm:text-sm text-xs text-neutral-400">with technolgies</p>
+        <Marquee className="sm:max-w-96 max-w-80">
         <div
-          className="flex items-center justify-center h-fit gap-8"
-          draggable="false"
+          className="flex items-center justify-center h-fit  w-full overflow-hidden mx-4 gap-8"
         >
-          <Image src={"/next.svg"} alt="stack" width={80} height={80} />
-          <Image src={"/drizzle.svg"} alt="stack" width={100} height={100} />
+          <Image src={"/next.svg"} alt="stack" width={80} height={80} draggable="false" />
+          <Image src={"/drizzle.svg"} alt="stack" width={100} height={100} draggable="false" />
           <Image
             src={"/hono.png"}
             alt="stack"
@@ -22,15 +23,17 @@ export default function Home() {
             height={100}
             quality={80}
             className="h-6 w-6"
+            draggable="false"
           />
-          <Image src={"/shadcn.svg"} alt="stack" width={22} height={22} />
-          <Image src={"/vercel.svg"} alt="stack" width={20} height={20} />
-          <Image src={"/neon.svg"} alt="stack" width={80} height={100} />
+          <Image src={"/shadcn.svg"} alt="stack" width={22} height={22} draggable="false" />
+          <Image src={"/vercel.svg"} alt="stack" width={20} height={20} draggable="false" />
+          <Image src={"/neon.svg"} alt="stack" width={80} height={100} draggable="false" />
         </div>
+        </Marquee>
       </div>
-      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+      <ol className="list-inside list-decimal text-xs sm:text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
         <li className="mb-2">
-          Get started by editing{" "}
+          Get started by editig{" "}
           <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
             app/page.tsx
           </code>
@@ -47,7 +50,7 @@ export default function Home() {
         <li>Deploy on vercel.🔥</li>
       </ol>
 
-      <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <div className="flex gap-4 items-center">
         <Button asChild>
           <a
             className="text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -67,7 +70,7 @@ export default function Home() {
         </Button>
         <Button asChild variant={"outline"}>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-black hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://github.com/tanavposwal/fyncstack"
             target="_blank"
             rel="noopener noreferrer"
